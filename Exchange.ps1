32525f6223e48033acd42d9aee5f807de4326654
Lee <lee.cook@live.com.au>
2024-07-04 11:59:59 +1000

    Update Exchange.ps1

--- Exchange.ps1
+++ Exchange.ps1
@@ -4,6 +4,9 @@ Get-MailboxPermission -identity "user@company.com"
 #Get current Calendar permissions for user@company.com calendar
 Get-MailboxFolderPermission -identity "user@company.com:\Calendar"
 
-#Set User "John.Legend" with Publishing Editor rights to user@company.com calendar.
-Set-MailboxFolderPermission -identity "user@company.com:\Calendar" -user "John.Legend" -AccessRights PublishingEditor
+#Add calendar permissions for user@company.com calendar
+Add-MailboxFolderPermission -identity "user@company.com:\Calendar" -user "John.Legend" -AccessRights Author
 
+#Update the User "John.Legend" to have Publishing Editor rights to user@company.com calendar.
+##This is used when the user already has calendar access but the access level needs to be changed.
+Set-MailboxFolderPermission -identity "user@company.com:\Calendar" -user "John.Legend" -AccessRights PublishingEditor
