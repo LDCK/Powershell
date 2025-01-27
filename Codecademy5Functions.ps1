72c07e52f610e4229d568c601863ae72da26c0f6
Lee <lee.cook@live.com.au>
2025-01-27 14:57:06 +1100

    Create Codecademy5Functions.ps1

---
+++ Codecademy5Functions.ps1
@@ -0,0 +1,12 @@
+# ----------
+Functions
+# ----------
+Functions in Powershell are groups of code collected in a single location that can be referenced anywhere in your code.
+Functions are a way to collect code in a single space and reference them with a simple command throughout your script.
+This makes code easier to read.
+
+function greet {
+  Write-Host "Hello, there!"
+}
+
+
